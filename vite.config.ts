e97415ad77d4b1c135b4d82be29498f2b169dac2
scarletea <fsljq99@outlook.com>
2024-08-31 19:07:20 +1000

    Make it an electron app.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
   },
   plugins: [react()],
   server: {
-    port: 3000,
     open: true
   }
 })
