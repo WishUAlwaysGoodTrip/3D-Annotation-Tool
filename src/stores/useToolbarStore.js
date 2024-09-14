508b134511130238d97270dab0cc7ad7f250816b
scarletea <fsljq99@outlook.com>
2024-09-15 03:09:01 +1000

    Persistence of mode and activeButton state.

--- src/stores/useToolbarStore.js
+++ src/stores/useToolbarStore.js
@@ -1,26 +1,20 @@
 import {create} from 'zustand';
-//import { persist } from 'zustand/middleware'
+import {persist} from "zustand/middleware";
 
 
-const useToolbarStore = create((set) => ({
-    mode: 'dragging', // initial state
-    activeButton: 'button4',
-    setMode: (newMode) => set({ mode: newMode }), // function to update state
-    setActiveButton: (activeButton) => set({ activeButton: activeButton }),
-  })
+const useToolbarStore = create(
+    persist(
+        (set) => ({
+            mode: 'dragging', // initial state
+            activeButton: 'button4',
+            setMode: (newMode) => set({ mode: newMode }), // function to update state
+            setActiveButton: (activeButton) => set({ activeButton: activeButton }),
+        }),
+        {
+            name: 'toolbar-storage',
+        }
+    )
 );
-  /*
-  persist(
-    (set) => ({
-      mode: 'dragging', // initial state
-      setMode: (newMode) => set({ mode: newMode }), // function to update state
-    }),
-    {
-      name: 'mode-storage', // name of the item in the storage
-      Storage: () => sharedStore,
-    }
-  )
-);
-*/
+
 
 export default useToolbarStore;
