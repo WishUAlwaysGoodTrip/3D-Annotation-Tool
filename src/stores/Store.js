d788ebdebbf445a85dffaaccdf6a34749eda762d
scarletea <fsljq99@outlook.com>
2024-10-11 22:45:38 +1100

    Update communication logic, accelerate the persist storage of state.

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -1,3 +1,5 @@
+//Store.js
+import debounce from 'lodash/debounce';
 import Store from 'electron-store';
 
 const electronStore = new Store();
@@ -7,12 +9,12 @@ const persistStorage = {
         const value = electronStore.get(name);
         return value !== undefined ? JSON.stringify(value) : null;
     },
-    setItem: (name, value) => {
+    setItem: debounce((name, value) => {
         electronStore.set(name, JSON.parse(value));
-    },
-    removeItem: (name) => {
+    }, 300), // 设置一个300ms的延迟
+    removeItem: debounce((name) => {
         electronStore.delete(name);
-    },
+    }, 300),
 };
 
 export default persistStorage;
