c72347868ad2e8a04747d64777a25e2c38d3fb7a
scarletea <fsljq99@outlook.com>
2024-10-29 23:05:22 +1100

    Adjust very very few comments.

--- src/Component/Render.jsx
+++ src/Component/Render.jsx
@@ -1089,9 +1089,9 @@ function updateControls() {
     }
     console.log("render drag")
   } else {
-    controls.enableRotate = true; // 禁止旋转
-    controls.enableZoom = true;   // 禁止缩放
-    controls.enablePan = true;    // 禁止平移
+    controls.enableRotate = true;
+    controls.enableZoom = true;
+    controls.enablePan = true;
     controls.mouseButtons = {
       MIDDLE: THREE.MOUSE.ROTATE,
       RIGHT: THREE.MOUSE.PAN
